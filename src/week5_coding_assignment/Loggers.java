ae023d1b11cddc7da10208412f6d36ac9105b389
Travis Stokes <tstokes433@gmail.com>
2022-10-06 16:00:55 -0500

    Incorrect class

--- src/week5_coding_assignment/Loggers.java
+++
@@ -1,8 +0,0 @@
-package week5_coding_assignment;
-
-public interface Loggers {
-public void info(String info);
-public void warning(String warning);
-public void error(String error);
-public void fatal(String fatal);
-}
